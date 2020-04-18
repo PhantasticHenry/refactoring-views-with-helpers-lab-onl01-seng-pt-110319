fe59b6ef7721433dadb5f2b7128debaae84bf008
github username <github email address>
2020-04-18 02:31:45 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    if song.artist_id == nil
+      edit_song_path(song) + "Add Artist"
+    else 
+      artist_path(song.artist_id) + "#{song.artist.name}"
+    end
+  end
 end
